8b1b9da3b941ff60f26c9a485b919e6df8c0a558
vipin <vipin@cbio.mskcc.org>
2014-05-27 11:59:00 -0700

    cluster - deleting unfinished or broken jobs

---
+++ cluster_job_del.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+# check the input arguments 
+if [ $# -eq 0 ] ; then
+    echo 'error: Please mention the Job ID to be delted'
+    exit 1 
+fi 
+
+# check the job number
+JOB_NUM='^[0-9]+$' 
+if ! [[ ${1} =~ $JOB_NUM ]] ; then 
+    echo 'error: Job ID NOT a number'
+    exit 1 
+fi
+
+# get the job id
+JOB_NUM_START=${1}
+shift 
+
+# check is there a second argument 
+if [ "$1" = "" ]; then
+    JOB_NUM_STOP=${JOB_NUM_START}
+else 
+    if ! [[ ${1} =~ $JOB_NUM ]] ; then 
+        echo 'error: Job ID NOT a number'
+        exit 1 
+    fi
+    JOB_NUM_STOP=${1}
+fi     
+
+# check the job range 
+if ! [[ "${JOB_NUM_STOP}" -ge ${JOB_NUM_START} ]] ; then 
+    echo error: Range value should be in ascending order $JOB_NUM_START $JOB_NUM_STOP
+    exit 1 
+fi 
+
+# delete the jobs
+for JOB_ID in `seq ${JOB_NUM_START} 1 ${JOB_NUM_STOP}`
+do 
+    echo deleting job $JOB_ID
+    qdel $JOB_ID
+done 
+
