38546ea7a06acb39c0c774e43df44249e30fc270
Vipin <vipin@cbio.mskcc.org>
2014-10-27 14:22:21 -0400

    checking the free memory bound

---
+++ fill_mem_check.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 
+"""
+check the free memmory available on the machine 
+usage: fill_mem_check.py <number-of-gigabytes>
+"""
+
+import sys 
+import time 
+
+if len(sys.argv) != 2:
+    print __doc__
+    sys.exit()
+
+count = int(sys.argv[1])
+gigabyte = (0,) * (1024 * 1024 * 1024 / 8)
+
+data = gigabyte * count 
+
+while True:
+    print "getting %d GB memory for use" % count  
+    time.sleep(1)
